1a3187808a7e84df01c14d6da06ee7537fd04887
lvmao520 <983084772@qq.com>
2022-10-25 15:57:10 +0800

    完成2-1的第一题的字符串数组声明

---
+++ src/com/yybd/base2_2/EnglishDemo01.java
@@ -0,0 +1,9 @@
+package com.yybd.base2_2;
+
+public class EnglishDemo01 {
+        public static void main(String[] args) {
+            //定义字符串数组,采用声明式定义,直接内容,不需要长度
+            String[] a={"Hello " , "Hello " , "Hello " , "Hello ","Hello "};
+            String[] b = {"Jack", "Tom", "Lee", "John", "Alisa"};
+        }
+    }
